ece8a4c0d21ba5f8c878d2385521783976d314ff
Ericsb52 <broadbent52@aol.com>
2022-04-21 14:43:42 -0600

    Eric Broadbent 4/21/22 added tumble weeds and barrels starting to work on powerups

---
+++ gunsmoke/tilemap/GS level 1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="GS level 1" tilewidth="16" tileheight="16" tilecount="190" columns="10">
+ <image source="../NES - GunSmoke - Level 01.png" width="160" height="304"/>
+</tileset>
